c18d1e429946e1b600b7c53ede98a8ef758e2984
sor <bosl95@naver.com>
2020-12-18 00:52:58 +0900

    refactor : 초기 설정 코드를 app으로 이동

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,12 @@
 package subway;
 
 import subway.controller.MainController;
+import subway.domain.DummyData;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        DummyData.load();
         MainController mainController = new MainController();
         mainController.run();
     }
